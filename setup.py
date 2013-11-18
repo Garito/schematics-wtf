f95acd7a469f7da9a6070bd42501e6c8b36d41b0
Neil Albrock <neil@atomised.coop>
2013-11-18 15:40:43 +0000

    Update to work with latest Schematics

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from setuptools import setup
 
 setup(
     name='schematics-wtf',
-    version='0.1.0-alpha',
+    version='0.1.1-alpha',
     license='BSD',
     description='Schematics Model to WTForm converter',
-    url='http://github.com/ryanolson/schematics-wtf',
+    url='http://github.com/neilalbrock/schematics-wtf',
     packages=['schematics_wtf'],
     install_requires = [
-        'schematics>=0.8.0-alpha',
+        'schematics>=0.9-3',
         'wtforms'
     ],
     dependency_links = [
-        'https://github.com/j2labs/schematics/tarball/master#egg=schematics-0.8.0-alpha'
+        'https://github.com/j2labs/schematics/tarball/master#egg=schematics-0.9-3'
     ],
     classifiers=[
         'Environment :: Other Environment',
